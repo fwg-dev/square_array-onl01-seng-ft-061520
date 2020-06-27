4c009b2f693a9fab2bf6ead9095d5f44f5b3be12
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-27 15:45:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,20 @@
-def square_array(array) # your code here
-square_array = []
-   array.each do |num|
-      square_array  << num * num
+# def square_array(array) # your code here
+# square_array = []
+#    array.each do |num|
+#       square_array  << num * num
+# 
+# end
+# return square_array
+# end
 
-end
-return square_array
-end
+numbers = [ 1,2,3]
+
+numbers.each {|number| return {square_array << number *number}}
+
+
+
+
+
 
 
 # array.each do |num|
